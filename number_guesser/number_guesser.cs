cfc46279081aaeb1ebecc2f97e44caf97104e1c4
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2024-09-01 22:10:51 +0200

    Create initial project structure
    This commit sets up the basic architecture for a new C# project named "number_guesser". It includes solution and project files, default settings, and basic implementation for the "Hello World" application.

---
+++ number_guesser/number_guesser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace number_guesser
+{
+    class number_guesser
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
